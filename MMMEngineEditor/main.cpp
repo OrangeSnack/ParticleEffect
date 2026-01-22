96a28eb08baedc662323d41c8d2775db57b7dc72
Foodie_Pudding <sacar1920@gmail.com>
2026-01-22 21:08:24 +0900

    [Renderer] RenderManager Structure Modify (#9)
    * main sync
    
    * Update .gitignore
    
    * Delete UserScripts_copy_1769013743926.dll
    
    * [ADD,FIX] RenderManager, MaterialSerializer, MeshSerializer
    
    Update Mungtengee
    
    * [FIX] RenderManager
    
    참조오류 해결
    
    ---------
    
    Co-authored-by: minkee009 <minkee5004@gmail.com>

--- MMMEngineEditor/main.cpp
+++ MMMEngineEditor/main.cpp
@@ -20,9 +20,6 @@
 #include "BuildManager.h"
 #include "DLLHotLoadHelper.h"
 
-//t삭제해야함
-#include "AssimpLoader.h"
-
 namespace fs = std::filesystem;
 using namespace MMMEngine;
 using namespace MMMEngine::Utility;
@@ -65,8 +62,8 @@ void Initialize()
 		BuildManager::Get().SetProgressCallbackString([](const std::string& progress) { std::cout << progress.c_str() << std::endl; });
 	}
 
-	RenderManager::Get().StartUp(&hwnd, windowInfo.width, windowInfo.height);
-	app->OnWindowSizeChanged.AddListener<RenderManager, &RenderManager::ResizeScreen>(&RenderManager::Get());
+	RenderManager::Get().StartUp(hwnd, windowInfo.width, windowInfo.height);
+	app->OnWindowSizeChanged.AddListener<RenderManager, &RenderManager::ResizeSwapChainSize>(&RenderManager::Get());
 
 	Microsoft::WRL::ComPtr<ID3D11Device> device = RenderManager::Get().GetDevice();
 	Microsoft::WRL::ComPtr<ID3D11DeviceContext> context = RenderManager::Get().GetContext();
